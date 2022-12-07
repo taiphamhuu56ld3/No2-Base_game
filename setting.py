40017095c79595a193e435c6e591e5a9dd75e1d8
TaiPham <115418952+taiphamhuu56ld3@users.noreply.github.com>
2022-12-07 23:29:30 +0700

    feat[setting]: add setting file

---
+++ setting.py
@@ -0,0 +1,17 @@
+level_map = [
+    '                            ',
+    '                            ',
+    '                            ',
+    ' XX    XXX            XX    ',
+    ' XX P                       ',
+    ' XXXX         XX         XX ',
+    ' XXXX       XX            X ',
+    ' XX    X  XXXX    XX  XX    ',
+    '       X  XXXX    XX  XXX   ',
+    '    XXXX  XXXXXX  XX  XXXX  ',
+    'XXXXXXXXXXXXXXXXXXXXXXXXXX  ', ]
+
+tile_size = 64
+screen_width = 1200
+# screen_height = 700
+screen_height = len(level_map) * tile_size
